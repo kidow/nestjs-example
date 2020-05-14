2080ceebb018504e8c5f3520a1479ebad2c2b96f
전산김동욱 <pg2@linkplanner.co.kr>
2020-05-14 15:28:33 +0900

    소스 정리

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(5000)
-}
-bootstrap()
+NestFactory.create<NestExpressApplication>(AppModule).then(app => app.listen(5000))
